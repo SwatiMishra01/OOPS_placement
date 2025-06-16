e804fb501ee207c78ef09eb7ca0cdc4056a71129
SwatiMishra01 <swati01mishra01@gmail.com>
2025-06-16 18:15:20 +0530

    1

--- 1_classes&objects.cpp
+++ 1_classes&objects.cpp
@@ -20,7 +20,19 @@ class Teacher{
         void ChangeDept(string newDept){
             dept=newDept;
         }
-        
+
+
+
+//to access private memeber we can use setter/getter functions
+
+//setter-private values  set krna
+        void SetSalary(double s){
+            salary=s;
+        }
+//getter-private value get krna
+        double GetSalary(){
+            return salary;
+        }
     };
 int main() {
     Teacher t1;
@@ -28,7 +40,10 @@ int main() {
     t1.dept="ece-ai";
     t1.subject="c++";
     t1.salary=20000;
-    cout << t1.name<<endl;
+    cout << t1.name<<endl;    //output-swati
+     cout << t1.salary<<endl; //output-cpp:7:12: note: declared private here
+    t1.SetSalary(500000);
+    cout<<GetSalary()>>endl;  //output-500000
 
     return 0;
 }
